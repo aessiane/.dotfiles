e7ca2e50379d34d668be4b86671a19ccedc2dc62
Abel Essiane <essiane.abel@gmail.com>
2024-04-15 10:37:41 +0100

    Installs treesitter parsers asynchronously

--- .config/nvim/lua/aessiane/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/aessiane/plugins/nvim-treesitter.lua
@@ -2,43 +2,49 @@
 -- import nvim-treesitter plugin safely
 local status, treesitter = pcall(require, "nvim-treesitter.configs")
 if not status then
-	return
+    return
 end
 
 -- configure treesitter
 treesitter.setup({
-	-- enable syntax highlighting
-	highlight = {
-		enable = true,
-	},
-	-- enable indentation
-	indent = { enable = true },
-	-- ensure these language parsers are installed
-	ensure_installed = {
-		"bash",
-		"css",
-		"csv",
-		"dockerfile",
-		"fish",
-		"git_config",
-		"git_rebase",
-		"gitcommit",
-		"gitignore",
-		"graphql",
-		"html",
-		"javascript",
-		"json",
-		"lua",
-		"markdown",
-		"markdown_inline",
-		"python",
-		"sql",
-		"terraform",
-		"toml",
-		"typescript",
-		"vim",
-		"yaml"
-	},
-	-- auto install above language parsers
-	auto_install = true,
+    -- enable syntax highlighting
+    highlight = {
+        enable = true,
+    },
+    -- enable indentation
+    indent = { enable = true },
+    -- ensure these language parsers are installed
+    ensure_installed = {
+        "bash",
+        "css",
+        "c",
+        "csv",
+        "dockerfile",
+        "fish",
+        "git_config",
+        "git_rebase",
+        "gitcommit",
+        "gitignore",
+        "graphql",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "rust",
+        "vimdoc",
+        "sql",
+        "terraform",
+        "toml",
+        "typescript",
+        "vim",
+        "yaml"
+    },
+    -- auto install above language parsers
+    auto_install = true,
+
+    -- install parsers synchronously
+    sync_install = false,
 })
